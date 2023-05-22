52ef4c21ac14ca02b6a364eb2c7f96ccca1d9d43
Hugo <hugosimoescontato@gmail.com>
2023-05-22 16:47:35 -0300

    configurando o knex e sqlite3

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,43 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-const users = [
-    'Hugo',
-    'Juli',
-    'Luke',
-    'Paulo',
-    'Claudia'
-]
-
-app.get('/users', (request, response) => {
-  const search = String(request.query.search);
-
-  const filteredUsers = search ? users.filter(user => users.includes(search)):users;
-
-  return response.json(filteredUsers);
-});
-
-app.get('/users/:id', (request, response) =>{
-  const id = Number(request.params.id);
-  const user = users[id];
-
-  return response.json(user);
-});
-
-app.post('/users', (request, response) => {
-  const data = request.body;
-
-  console.log(data);
-
-  const user = {
-    name: 'Hugo',
-    email: 'hugosimoescontato@gmail.com'
-  };
-
-  return response.json(user);
-});
+app.use(routes);
 
 app.listen(3333);
