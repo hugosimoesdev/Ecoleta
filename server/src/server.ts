a6c92d15d56402971ad70ed0b23618368de4de92
Hugo <hugosimoescontato@gmail.com>
2023-05-22 16:24:06 -0300

    configurando rotas

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,16 +2,42 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-  console.log('Listagem de Usuários');
+app.use(express.json());
 
-  response.json([
+const users = [
     'Hugo',
     'Juli',
     'Luke',
     'Paulo',
     'Claudia'
-  ]);
+]
+
+app.get('/users', (request, response) => {
+  const search = String(request.query.search);
+
+  const filteredUsers = search ? users.filter(user => users.includes(search)):users;
+
+  return response.json(filteredUsers);
+});
+
+app.get('/users/:id', (request, response) =>{
+  const id = Number(request.params.id);
+  const user = users[id];
+
+  return response.json(user);
+});
+
+app.post('/users', (request, response) => {
+  const data = request.body;
+
+  console.log(data);
+
+  const user = {
+    name: 'Hugo',
+    email: 'hugosimoescontato@gmail.com'
+  };
+
+  return response.json(user);
 });
 
 app.listen(3333);
